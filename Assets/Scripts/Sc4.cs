cf1f16173d403cb9f5ca8cf05a7f4b2ee3bb3b03
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2020-04-08 23:20:33 +0700

    test update

--- Assets/Scripts/Sc4.cs
+++ Assets/Scripts/Sc4.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using FancyScrollView.Example02;
+using UnityEditor.VersionControl;
 
 public class Sc4 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private ScrollView scrollView = default;
+	[SerializeField] private GameObject content = default;
+	[SerializeField] private Button prevCellButton = default;
+	[SerializeField] private Button nextCellButton = default;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public ItemData[] items;
+
+	private int m_index = 0;
+
+	private void Start()
+	{
+		prevCellButton.onClick.AddListener(scrollView.SelectPrevCell);
+		nextCellButton.onClick.AddListener(scrollView.SelectNextCell);
+		scrollView.OnSelectionChanged(OnSelectionChanged);
+
+		scrollView.UpdateData(items);
+		scrollView.SelectCell(0);
+	}
+
+	private void OnSelectionChanged(int index)
+	{
+		m_index = index;
+	}
 }
