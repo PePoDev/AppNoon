f3b2b5eccb54e74ae008362e0eeb72d54464bc8e
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2020-04-22 04:17:48 +0700

    fix

---
+++ Assets/Scripts/ClickStart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickStart : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown || Input.touchCount > 0)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
